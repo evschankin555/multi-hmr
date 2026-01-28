e9b2ded0414f849a819d5b8bc6e7bcdc983e5016
evschankin555 <evschankin555@gmail.com>
2026-01-28 14:50:35 +0300

    fix: абсолютные пути для интеграции с BodyBuilder2

--- utils/constants.py
+++ utils/constants.py
@@ -3,14 +3,21 @@
 # CC BY-NC-SA 4.0 license
 
 import os
+from pathlib import Path
 
-SMPLX_DIR = 'models'
-MEAN_PARAMS = 'models/smpl_mean_params.npz'
-CACHE_DIR_MULTIHMR = 'models/multiHMR'
+# Получаем абсолютный путь к директории multi-hmr
+# __file__ = tools/multi-hmr/utils/constants.py
+# parents[1] = tools/multi-hmr
+_MULTIHMR_ROOT = Path(__file__).resolve().parents[1]
 
-ANNOT_DIR = 'data'
-BEDLAM_DIR = 'data/BEDLAM'
-EHF_DIR = 'data/EHF'
-THREEDPW_DIR = 'data/3DPW'
+# Устанавливаем абсолютные пути
+SMPLX_DIR = str(_MULTIHMR_ROOT / 'models')
+MEAN_PARAMS = str(_MULTIHMR_ROOT / 'models' / 'smpl_mean_params.npz')
+CACHE_DIR_MULTIHMR = str(_MULTIHMR_ROOT / 'models' / 'multiHMR')
 
-SMPLX2SMPL_REGRESSOR = 'models/smplx/smplx2smpl.pkl'
+ANNOT_DIR = str(_MULTIHMR_ROOT / 'data')
+BEDLAM_DIR = str(_MULTIHMR_ROOT / 'data' / 'BEDLAM')
+EHF_DIR = str(_MULTIHMR_ROOT / 'data' / 'EHF')
+THREEDPW_DIR = str(_MULTIHMR_ROOT / 'data' / '3DPW')
+
+SMPLX2SMPL_REGRESSOR = str(_MULTIHMR_ROOT / 'models' / 'smplx' / 'smplx2smpl.pkl')
